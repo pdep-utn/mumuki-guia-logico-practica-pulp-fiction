fa84b4b3e0845e73dda045bf4347af04a1818cbc
gcrespi <crespi.gustavo@gmail.com>
2015-10-21 23:57:31 -0300

    Fix test

--- 00040_Pan_Y_Trabajo/test.pl
+++ 00040_Pan_Y_Trabajo/test.pl
@@ -4,6 +4,9 @@ test(es_sanCayetano_si_todos_sus_empleados_reciben_sus_encargos, nondet) :-
 test(es_sanCayetano_si_todos_sus_amigos_y_empleados_reciben_sus_encargos, nondet) :-
 	sanCayetano(franco).
 
+test(no_es_sanCayetano_si_alguno_de_sus_empleados_no_recibe_sus_encargos, fail) :-
+	sanCayetano(marsellus).
+
 test(estaEnProblemas_es_inversible,
-		set(Cayetano==[butch,donCorleone,franco,honeyBunny,mia,pumkin,winston])) :-
+		[true(Cayetano==donCorleone), nondet]) :-
 	sanCayetano(Cayetano).
