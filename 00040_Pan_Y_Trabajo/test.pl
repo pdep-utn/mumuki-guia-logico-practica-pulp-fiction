f76144c78f11ec1f599e2f11845dd2728d0bddcf
gcrespi <crespi.gustavo@gmail.com>
2015-10-21 23:01:44 -0400

    Mumuki Export on 2015-10-21 23:01:44 -0400

--- 00040_Pan_Y_Trabajo/test.pl
+++ 00040_Pan_Y_Trabajo/test.pl
@@ -4,10 +4,6 @@ test(es_sanCayetano_si_todos_sus_empleados_reciben_sus_encargos, nondet) :-
 test(es_sanCayetano_si_todos_sus_amigos_y_empleados_reciben_sus_encargos, nondet) :-
 	sanCayetano(franco).
 
-test(no_es_sanCayetano_si_alguno_de_sus_empleados_no_recibe_sus_encargos, fail) :-
-	sanCayetano(marsellus).
-
 test(estaEnProblemas_es_inversible,
-		[true(Cayetano==donCorleone), nondet]) :-
+		set(Cayetano==[butch,donCorleone,franco,honeyBunny,mia,pumkin,winston])) :-
 	sanCayetano(Cayetano).
-
